de1ab2ee8efcd82a6898b2801353bbc93cdf985f
hokto <hokuto.k1441@gmail.com>
2023-10-11 10:08:37 +0900

    ステップ2枠とピースの描画

--- tetris.py
+++ tetris.py
@@ -5,11 +5,14 @@ import pygame
 import random
 import math
 import time
+import copy
 from pygame.locals import *
 
+ROW = 20
+COL = 10
 HEIGHT = 580
 WIDTH = 720
-BLOCK_SIZE = 50
+BLOCK_SIZE = 15
 SURFACE = Rect(0,0,WIDTH,HEIGHT) # 画面のサイズ(x座標,y座標,幅,高さ)
 COLOR = {
     "Black":(0,0,0),
@@ -20,8 +23,42 @@ COLOR = {
     "Yellow":(255,255,0),
     "Orange":(255,130,0),
     "LightGray":(150,150,150),
-    "Gray":(50,50,50)}
+    "Gray":(50,50,50)
+    }
+BLOCK_COLOR =[
+    "Black",
+    "LightGray",
+    "LightBlue",
+    "Blue",
+    "Green",
+    "Orange",
+    "Red",
+    "Yellow",
+    "Gray"
+]
 
+# ステップ2:枠とピースの描画
+class Block:
+    def __init__(self,block_type,row,col):
+        self.shapes = [
+            [], # empty
+            [], # wall
+            [[0, -1], [0, 0], [0, 1], [0, 2]], # I block
+            [[-1, -1], [0, -1], [0, 0], [0, 1]], # J block
+            [[0, -1], [0, 0], [0, 1], [-1, 1]], # L block
+            [[0, -1], [0, 0], [-1, 0], [-1, 1]], # S blosk
+            [[-1, -1], [-1, 0], [0, 0], [0, 1]], # Z block
+            [[0, -1], [0, 0], [-1, 0], [0, 1]], # T block
+            [[0, 0], [-1, 0], [0, 1], [-1, 1]] # square
+        ]
+        self.block_type = block_type
+        self.shape = copy.deepcopy(self.shapes[self.block_type]) # こうしないと元の配列も変化する(参照コピー)
+        self.row = row
+        self.col = col
+    def draw(self,surface):
+        for block in self.shape:
+            pygame.draw.rect(surface,COLOR[BLOCK_COLOR[self.block_type]],Rect((self.col+block[1])*BLOCK_SIZE,(self.row+block[0])*BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE))
+        
 # ゲーム終了関数
 def game_exit():
     pygame.quit()
@@ -30,10 +67,20 @@ def game_exit():
 def main():
     pygame.init()
     surface = pygame.display.set_mode(SURFACE.size)
+    block = Block(4,1,5)
+    board = [[0 for j in range(COL+2)] for i in range(ROW+3)]
+    for col in range(COL+2):
+        board[-1][col] = 1
+    for row in range(ROW+3):
+        board[row][0] = 1
+        board[row][-1] = 1
     while True:
         surface.fill(COLOR["Black"])
-        # ステップ1:四角形の描画
-        pygame.draw.rect(surface,COLOR["LightGray"],(100,100,BLOCK_SIZE,BLOCK_SIZE))
+        for row in range(ROW+3):
+            for col in range(COL+2):
+                pygame.draw.rect(surface,COLOR[BLOCK_COLOR[board[row][col]]],Rect(col*BLOCK_SIZE,row*BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE))
+                
+        block.draw(surface)
         pygame.display.update()
         pressed_keys = pygame.key.get_pressed() # 押されたキー情報を取得
         for event in pygame.event.get():
